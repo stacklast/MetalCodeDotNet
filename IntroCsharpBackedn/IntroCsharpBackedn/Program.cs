435a0b1d9f6507e51ac54dde0b66844cc7517981
Edwin Benalcázar Espín <softwareywebsoluciones@gmail.com>
2025-07-19 10:40:50 -0500

    Aplicando Herencia

--- IntroCsharpBackedn/IntroCsharpBackedn/Program.cs
+++ IntroCsharpBackedn/IntroCsharpBackedn/Program.cs
@@ -2,9 +2,32 @@
 //Sale sale = new ();
 sale.Total = 15;
 
-var message = sale.GetInfo();
+var saleWithTax = new SaleWithTax(15, 1.16m);
+
+var message = saleWithTax.GetInfo();
 
 Console.WriteLine(message);
+
+class SaleWithTax: Sale
+{
+    public decimal Tax { get; set; }
+    public SaleWithTax(decimal total, decimal tax): base(total)
+    {
+        Tax = tax;
+    }
+
+    //override metodo del padre con virtual
+    public override string GetInfo()
+    {
+        return "El total es " + Total + " Impuesto: " + Tax;
+    }
+
+    //sobrecarga metodos con el mismo nombre en la misma clase
+    public string GetInfo(string message)
+    {
+        return message;
+    }
+}
 
 class Sale
 {
@@ -17,8 +40,9 @@ class Sale
         _some = 8;
     }
 
-    public string GetInfo()
+    //virtual para sobreescritura
+    public virtual string GetInfo()
     {
-        return "El total es " + Total;
+        return "El total es " + Total + _some;
     }
 }
