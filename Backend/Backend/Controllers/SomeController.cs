6a96a98d61d3068ac61c144a92c8399aaabb6a71
Edwin Benalcázar Espín <softwareywebsoluciones@gmail.com>
2025-07-19 17:03:29 -0500

    Uso de Task

--- Backend/Backend/Controllers/SomeController.cs
+++ Backend/Backend/Controllers/SomeController.cs
@@ -8,8 +8,8 @@ namespace Backend.Controllers
     [ApiController]
     public class SomeController : ControllerBase
     {
-        [HttpGet("async")]
-        public IActionResult GetAsync()
+        [HttpGet("sync")]
+        public IActionResult Getsync()
         {
             Stopwatch stopwatch = Stopwatch.StartNew();
             stopwatch.Start();
@@ -25,6 +25,28 @@ namespace Backend.Controllers
             stopwatch.Stop();
 
             return Ok(stopwatch.Elapsed);
+        }
+
+        [HttpGet("async")]
+        public async Task<IActionResult> GetAsync()
+        {
+            var task1 = new Task<int>(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("connexion a base de datos");
+
+                return 8;
+            });
+
+            task1.Start();
+
+            Console.WriteLine("hago otra cosa");
+
+            var result = await task1;
+
+            Console.WriteLine("Todo ha terminado");
+
+            return Ok(result);
         }
     }
 }
