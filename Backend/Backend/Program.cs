77b1d42c36859c1a2206b2fdd3d0a82643e0ac37
Edwin Benalcázar Espín <softwareywebsoluciones@gmail.com>
2025-07-19 16:13:29 -0500

    Corrigiendo Inyeccion de Dependencia

--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -1,6 +1,9 @@
+using Backend.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSingleton<IPeopleService, PeopleService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
