ffdd998007b0f063477e1631a9af5555254dfec5
Edwin Benalcázar Espín <softwareywebsoluciones@gmail.com>
2025-07-19 15:19:04 -0500

    Respuesta con un tipo de dato

---
+++ Backend/Backend/Controllers/PeopleController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        [HttpGet("alll")]
+        public List<People> GetPeople() => Respository.people;
+    }
+
+    public class Respository
+    {
+        public static List<People> people = new List<People>()
+        {
+            new People()
+            {
+                Id = 1,
+                Name = "Juan",
+                BirthDay = new DateTime(1990, 12, 3),
+            },
+            new People()
+            {
+                Id = 2,
+                Name = "Ana",
+                BirthDay = new DateTime(1990, 12, 3),
+            },
+            new People()
+            {
+                Id = 3,
+                Name = "Luis",
+                BirthDay = new DateTime(1990, 12, 3),
+            },
+            new People()
+            {
+                Id = 4,
+                Name = "Pedro",
+                BirthDay = new DateTime(1990, 12, 3),
+            },
+        };
+    }
+
+    public class People
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDay { get; set; }
+    }
+}
