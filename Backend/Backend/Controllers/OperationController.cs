43ebda316f44d7bd7feb867b0bcea70f8d4a465f
Edwin Benalcázar Espín <softwareywebsoluciones@gmail.com>
2025-07-19 13:15:59 -0500

    Nuevo Backend API proyecto

---
+++ Backend/Backend/Controllers/OperationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationController : ControllerBase
+    {
+        [HttpGet]
+        public decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+    }
+}
