37a1acb001dd980b107c2c9c26da5ef5dfd832ee
Edwin Benalcázar Espín <softwareywebsoluciones@gmail.com>
2025-07-19 15:03:55 -0500

    Uso de controlladores

--- Backend/Backend/Controllers/OperationController.cs
+++ Backend/Backend/Controllers/OperationController.cs
@@ -8,7 +8,29 @@ namespace Backend.Controllers
     public class OperationController : ControllerBase
     {
         [HttpGet]
-        public decimal Add(decimal a, decimal b)
+        public decimal Get(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        [HttpPost]
+        public decimal Add(decimal a, decimal b, 
+            [FromHeader] string Host, 
+            [FromHeader(Name = "Content-Lenght")] string ContentLength)
+        {
+            Console.WriteLine(Host);
+            Console.WriteLine(ContentLength);
+            return a + b;
+        }
+
+        [HttpPut]
+        public decimal Edit(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        [HttpDelete]
+        public decimal Delete(decimal a, decimal b)
         {
             return a + b;
         }
