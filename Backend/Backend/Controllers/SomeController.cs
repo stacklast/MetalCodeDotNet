17e0656147337b070886d9a2d771cb2e91ce092b
Edwin Benalcázar Espín <softwareywebsoluciones@gmail.com>
2025-07-19 17:08:45 -0500

    Medicion de Sync y Async

--- Backend/Backend/Controllers/SomeController.cs
+++ Backend/Backend/Controllers/SomeController.cs
@@ -30,23 +30,36 @@ namespace Backend.Controllers
         [HttpGet("async")]
         public async Task<IActionResult> GetAsync()
         {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            stopwatch.Start();
+
             var task1 = new Task<int>(() =>
             {
                 Thread.Sleep(1000);
                 Console.WriteLine("connexion a base de datos");
 
-                return 8;
+                return 1;
+            });
+
+            var task2 = new Task<int>(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Envio de correo");
+
+                return 2;
             });
 
             task1.Start();
+            task2.Start();
 
             Console.WriteLine("hago otra cosa");
 
             var result = await task1;
+            var result2 = await task2;
 
             Console.WriteLine("Todo ha terminado");
 
-            return Ok(result);
+            return Ok(result + " " + result2 + " " + stopwatch.Elapsed);
         }
     }
 }
