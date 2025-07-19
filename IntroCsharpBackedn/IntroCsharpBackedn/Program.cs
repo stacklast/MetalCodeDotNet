8061991f241493bc4c28d3646388565fc9a478fb
Edwin Benalcázar Espín <softwareywebsoluciones@gmail.com>
2025-07-18 22:12:04 -0500

    Creacion de Objectos

---
+++ IntroCsharpBackedn/IntroCsharpBackedn/Program.cs
@@ -0,0 +1,24 @@
+﻿var sale = new Sale(15);
+//Sale sale = new ();
+sale.Total = 15;
+
+var message = sale.GetInfo();
+
+Console.WriteLine(message);
+
+class Sale
+{
+    public decimal Total { get; set; }
+    private decimal _some;
+
+    public Sale(decimal total)
+    {
+        Total = total;
+        _some = 8;
+    }
+
+    public string GetInfo()
+    {
+        return "El total es " + Total;
+    }
+}
