e23bdc3c901f8cdec51ff33d6eb96093545665d8
Edwin Benalcázar Espín <softwareywebsoluciones@gmail.com>
2025-07-19 11:43:17 -0500

    Serializacion y Deserializacion de objetos json

---
+++ IntroCsharpBackedn/IntroCsharpBackedn/Generics.cs
@@ -0,0 +1,92 @@
+﻿/*
+ var numbers = new MyList<int>(5);
+var names = new MyList<string>(5);
+var beers = new MyList<Beer>(3);
+
+numbers.Add(1);
+numbers.Add(2);
+numbers.Add(3);
+numbers.Add(4);
+numbers.Add(5);
+numbers.Add(6);
+
+Console.WriteLine(numbers.GetContent());
+
+names.Add("nombres1");
+names.Add("nombres2");
+names.Add("nombres3");
+names.Add("nombres4");
+names.Add("nombres5");
+names.Add("nombres6");
+
+Console.WriteLine(names.GetContent());
+
+beers.Add(new Beer()
+{
+     Name = "Erdinger",
+     Price = 5
+});
+
+beers.Add(new Beer()
+{
+    Name = "Corona",
+    Price = 5
+});
+
+beers.Add(new Beer()
+{
+    Name = "Pilsener",
+    Price = 5
+});
+
+beers.Add(new Beer()
+{
+    Name = "Club",
+    Price = 5
+});
+
+Console.WriteLine(beers.GetContent());
+
+Console.ReadKey();
+
+public class MyList<T>
+{
+    private List<T> _list;
+    private int _limit;
+
+    public MyList(int limit) 
+    { 
+        _limit = limit;
+        _list = new List<T>();
+    }
+
+    public void Add(T element)
+    {
+        if (_list.Count < _limit)
+        {
+            _list.Add(element);
+        }
+    }
+
+    public string GetContent()
+    {
+        string content = "";
+        foreach (var element in _list)
+        {
+            content += element + ", ";
+        }
+        return content;
+    }
+}
+
+public class Beer
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+
+    public override string ToString()
+    {
+        return "Name: " + Name + " Price: "+ Price;
+    }
+}
+ */
--- IntroCsharpBackedn/IntroCsharpBackedn/Program.cs
+++ IntroCsharpBackedn/IntroCsharpBackedn/Program.cs
@@ -1,90 +1,30 @@
-﻿var numbers = new MyList<int>(5);
-var names = new MyList<string>(5);
-var beers = new MyList<Beer>(3);
+﻿using System.Text.Json;
 
-numbers.Add(1);
-numbers.Add(2);
-numbers.Add(3);
-numbers.Add(4);
-numbers.Add(5);
-numbers.Add(6);
-
-Console.WriteLine(numbers.GetContent());
-
-names.Add("nombres1");
-names.Add("nombres2");
-names.Add("nombres3");
-names.Add("nombres4");
-names.Add("nombres5");
-names.Add("nombres6");
-
-Console.WriteLine(names.GetContent());
-
-beers.Add(new Beer()
+var edwin = new People()
 {
-     Name = "Erdinger",
-     Price = 5
-});
+    Name = "Edwin",
+    Age = 666,
+};
 
-beers.Add(new Beer()
-{
-    Name = "Corona",
-    Price = 5
-});
+string json = JsonSerializer.Serialize(edwin);
 
-beers.Add(new Beer()
-{
-    Name = "Pilsener",
-    Price = 5
-});
+Console.WriteLine(json);
 
-beers.Add(new Beer()
-{
-    Name = "Club",
-    Price = 5
-});
+string myJson = @"{
+    ""Name"":""Juan"",
+    ""Age"":666
+    }";
 
-Console.WriteLine(beers.GetContent());
+People? juan = JsonSerializer.Deserialize<People>(myJson);
+
+Console.WriteLine(juan?.Name);
+Console.WriteLine(juan?.Age);
 
 Console.ReadKey();
 
-public class MyList<T>
-{
-    private List<T> _list;
-    private int _limit;
-
-    public MyList(int limit) 
-    { 
-        _limit = limit;
-        _list = new List<T>();
-    }
-
-    public void Add(T element)
-    {
-        if (_list.Count < _limit)
-        {
-            _list.Add(element);
-        }
-    }
-
-    public string GetContent()
-    {
-        string content = "";
-        foreach (var element in _list)
-        {
-            content += element + ", ";
-        }
-        return content;
-    }
-}
-
-public class Beer
+public class People
 {
     public string Name { get; set; }
-    public decimal Price { get; set; }
+    public int Age { get; set; }
 
-    public override string ToString()
-    {
-        return "Name: " + Name + " Price: "+ Price;
-    }
 }
