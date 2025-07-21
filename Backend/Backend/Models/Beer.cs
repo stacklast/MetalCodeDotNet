6589b9c4d0f60a9046438199aaa4b54bf09a3d6d
Edwin Benalcázar Espín <softwareywebsoluciones@gmail.com>
2025-07-20 19:49:30 -0500

    Instalacion Entity Framework y agregado de  Modelos

---
+++ Backend/Backend/Models/Beer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend.Models
+{
+    public class Beer
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BeerID { get; set; }
+        public string Name { get; set; }
+        public int BrandID { get; set; }
+
+        [ForeignKey("BrandID")]
+        public virtual Brand Brand { get; set; }
+    }
+}
