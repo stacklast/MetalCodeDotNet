c69f01be992fcf4945a9edc9a7483fb765f8bf6b
Edwin <softwareywebsoluciones@gmail.com>
2025-07-26 00:17:33 -0500

    Creacion de Dtos

---
+++ Backend/Backend/DTOs/BeerInsertDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend.DTOs
+{
+    public class BeerInsertDto
+    {
+        public string Name { get; set; }
+        public int BrandID { get; set; }
+        public decimal Alcohol { get; set; }
+    }
+}
