ab30726686df44ec38a79937e5bcc6361b1788bf
Edwin <softwareywebsoluciones@gmail.com>
2025-07-26 00:12:51 -0500

    Nuevo campo migrado

--- Backend/Backend/Models/Beer.cs
+++ Backend/Backend/Models/Beer.cs
@@ -11,6 +11,9 @@ namespace Backend.Models
         public string Name { get; set; }
         public int BrandID { get; set; }
 
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Alcohol { get; set; }
+
         [ForeignKey("BrandID")]
         public virtual Brand Brand { get; set; }
     }
