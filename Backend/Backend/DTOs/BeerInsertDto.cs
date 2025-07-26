742d0378f5cbae90ae5a63631f5a6669e6461bb0
Edwin <softwareywebsoluciones@gmail.com>
2025-07-26 18:45:43 -0500

    Validaciones y mensajes personalizador.

--- Backend/Backend/DTOs/BeerInsertDto.cs
+++ Backend/Backend/DTOs/BeerInsertDto.cs
@@ -2,7 +2,7 @@
 {
     public class BeerInsertDto
     {
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public int BrandID { get; set; }
         public decimal Alcohol { get; set; }
     }
--- Backend/Backend/Validators/BeerInsertValidator.cs
+++ Backend/Backend/Validators/BeerInsertValidator.cs
@@ -7,9 +7,11 @@ namespace Backend.Validators
     {
         public BeerInsertValidator() 
         {
-            RuleFor(x=> x.Name).NotEmpty();    
-
+            RuleFor(x=> x.Name).NotEmpty().WithMessage("El Nombre es obligatorio");
+            RuleFor(x => x.Name).Length(2, 20).WithMessage("El Nombre debe medir de 2 a 20 caracteres");
+            RuleFor(x => x.BrandID).NotNull().WithMessage(x=>"La marca es obligatoria");
+            RuleFor(x => x.BrandID).GreaterThan(0).WithMessage("Error con el valor enviado en marca");
+            RuleFor(x => x.Alcohol).GreaterThan(0).WithMessage("El {PropertyName} debe ser mayor a 0");
         }
-
     }
 }
