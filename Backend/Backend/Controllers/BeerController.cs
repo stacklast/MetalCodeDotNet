c0dde034c015ea6bf370bc3e747b95dffd9f6e9a
Edwin <softwareywebsoluciones@gmail.com>
2025-07-26 20:25:53 -0500

    Eliminar informacion DELETE

--- Backend/Backend/Controllers/BeerController.cs
+++ Backend/Backend/Controllers/BeerController.cs
@@ -103,5 +103,22 @@ namespace Backend.Controllers
             return Ok(beerDto);
         }
 
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var beer = await _context.Beers.FindAsync(id);
+
+            if (beer == null)
+            {
+                return NotFound();
+            }
+
+            _context.Beers.Remove(beer);
+            await _context.SaveChangesAsync();
+
+            //return NoContent();
+            return Ok();
+        }
+
     }
 }
